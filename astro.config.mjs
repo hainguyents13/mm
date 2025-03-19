4b3e5ced75229c8cb7c28397d95d03c2f89d5b9c
Hải Nguyễn <hainguyen.ts13@gmail.com>
2025-03-19 16:38:56 +0700

    ✨ (astro.config.mjs): add base path configuration for deployment to support subdirectory hosting 📝 (Layout.astro): update SEO description to reflect the new branding and product offerings

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,5 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://markingmate.tw",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
+  base: "/mm/",
 });

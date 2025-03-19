79adf091f23bc0676fa27a0e83e354da6a19d957
Hải Nguyễn <hainguyen.ts13@gmail.com>
2025-03-19 23:45:29 +0700

    Create astro.config.mjs

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://markingmate.tw",
+  integrations: [tailwind(), mdx(), sitemap(), icon()],
+  base: "/mm/",
+});

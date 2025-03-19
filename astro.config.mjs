f869de06aa2828164f2f49d50131d51f9e39734c
Hải Nguyễn <hainguyen.ts13@gmail.com>
2025-03-19 16:35:19 +0700

    remove astroship name

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://markingmate.tw",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
